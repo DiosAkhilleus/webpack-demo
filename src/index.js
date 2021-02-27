3f9a9fe2dc66730dcb59105009608e4c71b4f309
DiosAkhilleus <abertin1809@gmail.com>
2021-02-27 12:40:31 -0600

    testing webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div');
+
+
+    element.innerHTML = myName('Andrew');
+    return element;
+}
+
+document.body.appendChild(component());
